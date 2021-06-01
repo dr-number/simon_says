0c33e37b7629fd8751c3d36e29571601abe18204
pc-97 <dr.number@yandex.ru>
2021-06-01 21:55:42 +0700

    deploy dir

--- src/main.js
+++ src/main.js
@@ -4,12 +4,6 @@ import App from './App.vue'
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/simon_says/'
-      : '/'
-}
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
