e472af572afe1daa494389ae49e8dbc826d544fa
pc-97 <dr.number@yandex.ru>
2021-06-01 17:56:07 +0700

    deploy

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.config.devtools = true
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/simon_says/'
+      : '/'
+}
 
 new Vue({
   render: h => h(App),
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-
-Vue.config.devtools = true
-
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/simon_says/'
-        : '/'
-}
